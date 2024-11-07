8f5f3e38383876aeba279ddcd1eafa74264d914d
andreiJJFS <57998115+andreiJJFS@users.noreply.github.com>
2024-11-07 12:54:38 +0000

    added elemental lamhs

--- Chaos/Formulae/Damage/DefaultDamageFormula.cs
+++ Chaos/Formulae/Damage/DefaultDamageFormula.cs
@@ -20,7 +20,7 @@ public class DefaultDamageFormula : IDamageFormula
         //mostly lifted from http://da-wizard.com/elements.html
         //                                                  D E F E N S E
         //                         None,  Fire,  Water,  Wind,  Earth, Holy,  Darkness,  Wood,  Metal,  Undead
-        /*      None*/     new[] { 1.00m, 0.90m, 0.90m,  0.90m, 0.90m, 1.70m, 1.70m,     0.37m, 0.37m,  0.37m }.ToImmutableArray(),
+        /*      None*/     new[] { 1.00m, 0.90m, 0.90m,  0.90m, 0.90m, 0.93m, 0.83m,     0.37m, 0.37m,  0.37m }.ToImmutableArray(),
         /* O    Fire*/     new[] { 1.10m, 1.00m, 0.75m,  1.25m, 1.00m, 0.93m, 0.83m,     2.01m, 0.83m,  2.01m }.ToImmutableArray(),
         /* F    Water*/    new[] { 1.10m, 1.25m, 1.00m,  1.00m, 0.75m, 0.93m, 0.83m,     0.58m, 1.74m,  0.93m }.ToImmutableArray(), 
         /* F    Wind*/     new[] { 1.10m, 0.75m, 1.00m,  1.00m, 1.25m, 0.93m, 0.83m,     1.03m, 1.74m,  0.83m }.ToImmutableArray(),
